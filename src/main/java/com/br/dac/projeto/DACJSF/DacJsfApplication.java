193c065cc41a391e7bf24d5ce9c2c365b96c57fe
milena.batista <milena.batista>
2022-11-09 14:48:45 -0300

    Inicializando projeto

---
+++ src/main/java/com/br/dac/projeto/DACJSF/DacJsfApplication.java
@@ -0,0 +1,13 @@
+package com.br.dac.projeto.DACJSF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DacJsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DacJsfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/dac/projeto/DACJSF/DacJsfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.dac.projeto.DACJSF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DacJsfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
